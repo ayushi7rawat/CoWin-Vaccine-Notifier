165551ffff30ac35402bf36510275e37f88fc5d4
Dhamodaran Pandiyan <18031856+dhamodaran-pandiyan@users.noreply.github.com>
2021-05-16 11:39:39 +0530

    updated script.py
    1. Fixed typo on docstring
    2. removed unused imports (import json)
    3. renamed the only camel-case variable (i.e. pinCodes. pinCode)  mentioned in the script to follow the naming standard.
    4.  removed unnecessary print statements (user can add if needed while debugging) on the script
    5.  removed unused variable: flag = False
    6. removed not-required "else: pass" statements. script will work the same without those statements.

--- script.py
+++ script.py
@@ -1,17 +1,15 @@
 '''
-Script: Covid Vaccine Slot Availablity Notifier
+Script: Covid Vaccine Slot Availability Notifier
 By Ayushi Rawat
 '''
 
 import requests
-
 from datetime import datetime, timedelta
 import time
 
-import json
 
 age = 52
-pinCodes = ["462003"]
+pincodes = ["462003"]
 num_days = 2
 
 print_flag = 'Y'
@@ -25,32 +23,24 @@ actual_dates = [i.strftime("%d-%m-%Y") for i in list_format]
 while True:
     counter = 0   
 
-    for pinCode in pinCodes:   
+    for pincode in pincodes:   
         for given_date in actual_dates:
 
-            URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pinCode, given_date)
+            URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, given_date)
             header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} 
             
             result = requests.get( URL, headers=header )
-            # print('-------------------------------------------------------------')
-            # print(result.text)
-            # print('-------------------------------------------------------------')
 
             if result.ok:
                 response_json = result.json()
 
-                flag = False
                 if response_json["centers"]:            
                     if(print_flag.lower() =='y'):
 
                         for center in response_json["centers"]:
-                            # print('-------------------------------------------------------------')
-                            # print(center)
-                            # print('-------------------------------------------------------------')
-
                             for session in center["sessions"]:
                                 if (session["min_age_limit"] <= age and session["available_capacity"] > 0 ) :
-                                    print('Pincode: ' + pinCode)
+                                    print('Pincode: ' + pincode)
                                     print("Available on: {}".format(given_date))
                                     print("\t", center["name"])
                                     print("\t", center["block_name"])
@@ -60,22 +50,14 @@ while True:
                                     if(session["vaccine"] != ''):
                                         print("\t Vaccine type: ", session["vaccine"])
                                     print("\n")
-
-                                    counter = counter + 1
-                                else:
-                                    pass                                    
-                else:
-                    pass        
-                          
+                                    counter = counter + 1        
             else:
                 print("No Response!")
-
                 
     if(counter == 0):
         print("No Vaccination slot avaliable!")
     else:
         print("Search Completed!")
-
 
     dt = datetime.now() + timedelta(minutes=3)
 
