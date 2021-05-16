7ddf253d2f03c0f180ff8af11153670103b7ed42
Siddharth Sharma <ssiddharth408@gmail.com>
2021-05-16 21:56:22 +0530

    changed hardcoded variables to inputs

--- script.py
+++ script.py
@@ -10,8 +10,9 @@ import time
 
 import json
 
-age = 52
-pinCodes = ["462003"]
+age = int(input("Enter Age : "))
+input_pincode = input("Enter Pincode : ")
+pinCodes = [input_pincode]
 num_days = 2
 
 print_flag = 'Y'
@@ -23,25 +24,23 @@ list_format = [actual + timedelta(days=i) for i in range(num_days)]
 actual_dates = [i.strftime("%d-%m-%Y") for i in list_format]
 
 while True:
-    counter = 0   
+    counter = 0
 
-    for pinCode in pinCodes:   
+    for pinCode in pinCodes:
         for given_date in actual_dates:
 
-            URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pinCode, given_date)
-            header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} 
-            
-            result = requests.get( URL, headers=header )
-            # print('-------------------------------------------------------------')
-            # print(result.text)
-            # print('-------------------------------------------------------------')
+            URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(
+                pinCode, given_date)
+            header = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
 
+            result = requests.get(URL, headers=header)
             if result.ok:
                 response_json = result.json()
 
                 flag = False
-                if response_json["centers"]:            
-                    if(print_flag.lower() =='y'):
+                if response_json["centers"]:
+                    if print_flag.lower() == 'y':
 
                         for center in response_json["centers"]:
                             # print('-------------------------------------------------------------')
@@ -49,7 +48,7 @@ while True:
                             # print('-------------------------------------------------------------')
 
                             for session in center["sessions"]:
-                                if (session["min_age_limit"] <= age and session["available_capacity"] > 0 ) :
+                                if session["min_age_limit"] <= age and session["available_capacity"] > 0:
                                     print('Pincode: ' + pinCode)
                                     print("Available on: {}".format(given_date))
                                     print("\t", center["name"])
@@ -57,25 +56,23 @@ while True:
                                     print("\t Price: ", center["fee_type"])
                                     print("\t Availablity : ", session["available_capacity"])
 
-                                    if(session["vaccine"] != ''):
+                                    if session["vaccine"] != '':
                                         print("\t Vaccine type: ", session["vaccine"])
                                     print("\n")
 
                                     counter = counter + 1
                                 else:
-                                    pass                                    
+                                    pass
                 else:
-                    pass        
-                          
+                    pass
+
             else:
                 print("No Response!")
 
-                
-    if(counter == 0):
+    if counter == 0:
         print("No Vaccination slot avaliable!")
     else:
         print("Search Completed!")
-
 
     dt = datetime.now() + timedelta(minutes=3)
 
